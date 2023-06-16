92a7dc06ec9242de9ce2b172ec5a8b8e7983f48d
ALESSANDRO MATIAS ARENA <46917132@comunidadort.edu.ar>
2023-06-16 10:32:15 -0300

    aaaa

---
+++ src/MisPresentaciones.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import NavBar from './componentes/navBar-iniciada'
+import Banner from './componentes/banner'
+import CardCurso from './componentes/cardCurso'
+import './App.css'
+
+export default function MisPresentaciones(){
+    return(
+        <div>
+            <NavBar></NavBar>
+        </div>
+    )
+}
+
