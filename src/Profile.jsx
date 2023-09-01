b9f9a52e54755ebef4540d9c393aea71ff58128f
Tobias Ariel Suez <sueztobiasariel@gmail.com>
2023-09-01 08:32:10 -0300

    perfil

--- src/Profile.jsx
+++ src/Profile.jsx
@@ -3,13 +3,41 @@ import NavBar from './componentes/navBar-iniciada'
 import Banner from './componentes/banner'
 import CardCurso from './componentes/cardCurso'
 import './App.css'
+import './Profile.css';
 
 
 
-function Profile(){
-    return(
+function Profile() {
+    return (
         <div>
-            <NavBar></NavBar>
+            <NavBar ></NavBar>
+            <div className="profile-page">
+                <div className="profile-header">
+                    <img src="https://example.com/avatar.png" alt="Avatar" />
+                    <h1>NOMBREE</h1>
+                </div>
+                <div className="profile-info">
+                    <h2>Información del perfil</h2>
+                    <p>
+                        biografia   
+                    </p>
+                    <ul>
+                        <li>
+                            <strong>Nombre:</strong> 
+                        </li>
+                        <li>
+                            <strong>Correo electrónico:</strong>
+                        </li>
+                        <li>
+                            <strong>Fecha de nacimiento:</strong> 
+                        </li>
+                    </ul>
+                </div>
+                <div className="profile-actions">
+                    <button>Editar perfil</button>
+                    <button>Cerrar sesión</button>
+                </div>
+            </div>
         </div>
     )
 }
