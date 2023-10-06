5f6d8e02ba8263719b07caa49741b0be60f268b9
ALESSANDRO MATIAS ARENA <46917132@comunidadort.edu.ar>
2023-10-06 09:33:18 -0300

    nashei

--- src/CrearCurso/MCrearCurso3.jsx
+++ src/CrearCurso/MCrearCurso3.jsx
@@ -4,10 +4,11 @@ import NavBar from '../componentes/navBar-iniciada.jsx';
 import { Link, useParams } from 'react-router-dom';
 import { useContext } from 'react';
 import { CursoContext } from './../../context/cursoContext';
+import Card from 'react-bootstrap/Card'; 
 import Modal from 'react-bootstrap/Modal'; // Importar el componente Modal
 import Button from 'react-bootstrap/Button';
-import {uploadfile} from '../Firebase/config'
-import {v4} from  'uuid'
+import { uploadfile } from '../Firebase/config'
+import { v4 } from 'uuid'
 function MCrearCurso3() {
 
   const { cursoG, setCursoG } = useContext(CursoContext);
@@ -18,19 +19,19 @@ function MCrearCurso3() {
   const [fileFoto, setFileFoto] = useState()
   const [fileVideo, setFileVideo] = useState()
   const [showModal, setShowModal] = useState(false); // Estado para controlar si se muestra el modal
-  
+
   const handleClose = () => {
     setShowModal(false);
   };
 
-  const handleShow = async() => {
+  const handleShow = async () => {
     const urlBanner = await uploadfile(fileBanner)
     console.log(urlBanner)
     const urlFoto = await uploadfile(fileFoto)
     const urlVideo = await uploadfile(fileVideo)
-    setCursoG({...cursoG, PortadaCurso: urlBanner})
-   // setCursoG({...cursoG, imagenes: urlFoto})
-   // setCursoG({...cursoG, videos: urlVideo})
+    setCursoG({ ...cursoG, PortadaCurso: urlBanner })
+    // setCursoG({...cursoG, imagenes: urlFoto})
+    // setCursoG({...cursoG, videos: urlVideo})
     console.log(cursoG)
     setShowModal(true);
   };
@@ -78,7 +79,7 @@ function MCrearCurso3() {
   const terminar = async () => {
     let cursoStringified = JSON.stringify(cursoG);
     try {
-      
+
       const response = await fetch('http://localhost:3000/MCrearCurso3', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
@@ -89,85 +90,85 @@ function MCrearCurso3() {
     } catch {
       throw new Error(`No se pudo realizar el fetch tipo POST :(`);
     }
-    
+
   };
 
   return (
     <div>
       <NavBar />
       <div className="three-rows-view">
-        <div className="row">
-          <h2 className="row-title">Banner:</h2>
-          <div className="photo-container">
-            {uploadedBanner ? (
-              <img src={uploadedBanner} alt="Banner" className="photo" />
-            ) : (
-              <p>No se ha seleccionado ningun banner</p>
-            )}
-          </div>
-          <input
-            type="file"
-            accept="banner/*"
-            onChange={handleBannerUpload}
-            id="banner-upload"
-            style={{ display: 'none' }}
-          />
-          <label htmlFor="banner-upload" className="custom-upload-button">
-            Seleccionar Banner
-          </label>
-        </div>
+        <Card className="upload-card">
+          <Card.Body>
+            <div className="row">
+              <h2 className="row-title">Banner:</h2>
+              <div className="photo-container">
+                {uploadedBanner ? (
+                  <img src={uploadedBanner} alt="Banner" className="photo" />
+                ) : (
+                  <p>No se ha seleccionado ningun banner</p>
+                )}
+              </div>
+              <input
+                type="file"
+                accept="banner/*"
+                onChange={handleBannerUpload}
+                id="banner-upload"
+                style={{ display: 'none' }}
+              />
+              <label htmlFor="banner-upload" className="custom-upload-button">
+                Seleccionar Banner
+              </label>
+            </div>
 
-        <div className="row">
-          <h2 className="row-title">Imagenes:</h2>
-          <div className="photo-container">
-            {uploadedImage ? (
-              <img src={uploadedImage} alt="Imagen" className="photo" />
-            ) : (
-              <p>No se ha seleccionado ninguna imagen</p>
-            )}
-          </div>
-          <input
-            type="file"
-            accept="image/*"
-            onChange={handleImageUpload}
-            id="image-upload"
-            style={{ display: 'none' }}
-          />
-          <label htmlFor="image-upload" className="custom-upload-button">
-            Seleccionar Imagen
-          </label>
-        </div>
+            <div className="row">
+              <h2 className="row-title">Imagenes:</h2>
+              <div className="photo-container">
+                {uploadedImage ? (
+                  <img src={uploadedImage} alt="Imagen" className="photo" />
+                ) : (
+                  <p>No se ha seleccionado ninguna imagen</p>
+                )}
+              </div>
+              <input
+                type="file"
+                accept="image/*"
+                onChange={handleImageUpload}
+                id="image-upload"
+                style={{ display: 'none' }}
+              />
+              <label htmlFor="image-upload" className="custom-upload-button">
+                Seleccionar Imagen
+              </label>
+            </div>
+            <div className="row">
+              <h2 className="row-title">Videos:</h2>
+              <div className="photo-container">
+                {uploadedVideo ? (
+                  <video controls>
+                    <source src={uploadedVideo} type="video/mp4" />
+                    Tu navegador no admite el elemento de video.
+                  </video>
+                ) : (
+                  <p>No se ha seleccionado ningún video</p>
+                )}
+              </div>
+              <input
+                type="file"
+                accept="video/*"
+                onChange={handleVideoUpload}
+                id="video-upload"
+                style={{ display: 'none' }}
+              />
+              <label htmlFor="video-upload" className="custom-upload-button">
+                Seleccionar Video
+              </label>
+            </div>
+            <Link className={`crear-curso-option`} onClick={handleShow}>
+              Terminar
+            </Link>
+          </Card.Body>
+        </Card>
 
-        <div className="row">
-          <h2 className="row-title">Videos:</h2>
-          <div className="photo-container">
-            {uploadedVideo ? (
-              <video controls>
-                <source src={uploadedVideo} type="video/mp4" />
-                Tu navegador no admite el elemento de video.
-              </video>
-            ) : (
-              <p>No se ha seleccionado ningún video</p>
-            )}
-          </div>
-          <input
-            type="file"
-            accept="video/*"
-            onChange={handleVideoUpload}
-            id="video-upload"
-            style={{ display: 'none' }}
-          />
-          <label htmlFor="video-upload" className="custom-upload-button">
-            Seleccionar Video
-          </label>
-        </div>
-
-        <Link
-          className={`crear-curso-option`}
-          onClick={handleShow}
-        >
-          Terminar
-        </Link>
       </div>
       <Modal show={showModal} onHide={handleClose}>
         <Modal.Header closeButton>
@@ -179,24 +180,22 @@ function MCrearCurso3() {
         <Modal.Footer>
           <Button>
 
-        <Link 
-        to="/"
-        variant="danger" 
-        onClick={terminar}
-        >
-            Confirmar
-          </Link>
+            <Link
+              to="/"
+              variant="danger"
+              onClick={terminar}
+            >
+              Confirmar
+            </Link>
           </Button>
           <Button>
-          <Link variant="danger" onClick={handleClose}>
-            Cerrar
-          </Link>
+            <Link variant="danger" onClick={handleClose}>
+              Cerrar
+            </Link>
           </Button>
         </Modal.Footer>
       </Modal>
     </div>
   );
 }
-
 export default MCrearCurso3;
-
