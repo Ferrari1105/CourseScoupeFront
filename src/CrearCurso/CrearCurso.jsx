ef849ed5062f27c16a37fec5bd6cc728aaab282c
Gabriel Guivisdalsky <gguivisdalsky@gmail.com>
2023-11-10 08:46:06 -0300

    sebugeo

--- src/CrearCurso/CrearCurso.jsx
+++ src/CrearCurso/CrearCurso.jsx
@@ -8,6 +8,7 @@ import './CrearCurso.css';
 import MisPresentaciones from './MisPresentaciones';
 import { useContext } from "react"
 import { UsuarioContext } from '../../context/usuarioContext';
+
 function CrearCurso() {
   const {usuarioG} = useContext(UsuarioContext)
   const [proceso, setProceso] = useState(null);
@@ -18,25 +19,27 @@ function CrearCurso() {
     
   }
   return (
+    <>
     <div>
       {usuarioG? (
-              <NavBarIniciada/>
+        <NavBarIniciada/>
             ) : (
               <NavBar/>
-            )}
+              )}
+    </div>
       <div className="crear-curso-container">
         <h2 className="crear-curso-title">
           Antes de comenzar a crear el curso, ¿cómo quieres que sea el proceso?
         </h2>
         <div className="botones-siguiente"> 
-          <Link to="/MCrearCurso" className={`boton-siguiente ${proceso === 'manual' ? 'active' : ''}`}onClick={() => crearCursoManual()}>Opcion Manual</Link>
+        <Link to="/MCrearCurso" className={`boton-siguiente ${proceso === 'manual' ? 'active' : ''}`} onClick={() => crearCursoManual()}>Opcion Manual</Link>
         <Link to="/CrearCursoIA" className={`boton-siguiente ${ proceso === 'automatica' ? 'active' : '' }`} onClick={() => setProceso('automatica')}>Opción Automática</Link>
         </div>
         <div className="crear-curso-info">
           <p className="crear-curso-info-text">Como saber qué opción elegir: Al elegir el modo manual, usted tendrá que ingresar los contenidos del curso y tendrá acceso a una inteligencia artificial como asistente que le dará recomendaciones sobre el contenido. Por otro lado, al elegir Inteligencia Artificial, usted le tendrá que decir a la inteligencia artificial qué debe incluir en el curso.</p>
         </div>
       </div>
-    </div>
+              </>
   );
 }
 
--- src/componentes/navBar-iniciada.jsx
+++ src/componentes/navBar-iniciada.jsx
@@ -53,13 +53,13 @@ function NavBar() {
               <Link to={"/Profile"}>
             <div className='cuadrado-perfil'>  
               <p className='usuario-navbar-Nombre'>{usuarioG?.NombreUsuario}</p>
-                <Image className='usuario-navbar-foto' src="https://pbs.twimg.com/media/EIOHEVlXkAE0QWW.jpg" rounded />
+                <Image className='usuario-navbar-foto' src={usuarioG?.FotoDePerfil} rounded />
             </div>
               </Link>
               <Link to={"/CarritoDeCompras"}>
                 
               <div className='carrito'>
-              <svg xmlns="http://www.w3.org/2000/svg" width="46" height="66" fill="white" class="bi bi-cart" viewBox="0 0 16 16">
+              <svg xmlns="http://www.w3.org/2000/svg" width="46" height="66" fill="white" className="bi bi-cart" viewBox="0 0 16 16">
                <path d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .491.592l-1.5 8A.5.5 0 0 1 13 12H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5zM3.102 4l1.313 7h8.17l1.313-7H3.102zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2zm7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2z"/>
               </svg>
                <Badge pill bg="danger" className='notificaciones'>{cantCarrito.length || 0}</Badge>
